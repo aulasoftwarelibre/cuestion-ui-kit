2a5b550647d9a0bc4bc09e873fde902978392e0f
Sergio Gómez <sergio@uco.es>
2019-06-28 20:25:08 +0200

    Removed example button

--- .storybook/config.tsx
+++ .storybook/config.tsx
@@ -1,18 +1,14 @@
-
 import { setOptions } from "@storybook/addon-options";
 import { addDecorator, configure } from "@storybook/react";
-import themeDecorator from "./theme-decorator";
 
 setOptions({
   addonPanelInRight: true,
-  name: "React UI Kit",
+  name: "React UI Kit"
 });
 
 const req = require.context("../src", true, /.stories.tsx$/);
 function loadStories() {
   req.keys().forEach((filename: string) => req(filename));
 }
-
-addDecorator(themeDecorator);
 
 configure(loadStories, module);
--- src/utils/themed.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import * as renderer from "react-test-renderer";
-import { ThemeProvider } from "styled-components";
-import theme from "../theme";
-
-const themed = (component: React.ReactElement) =>
-  renderer.create(<ThemeProvider theme={theme}>{component}</ThemeProvider>);
-
-export default themed;
