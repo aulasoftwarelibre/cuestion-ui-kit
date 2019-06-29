a653a6c5ca4d14746eea9a8b961242e3e9aa8405
Sergio Gómez <sergio@uco.es>
2019-06-29 18:30:20 +0200

    Added session input code component (#1)
    * Added new component to insert code session
    * Updated license
    * Code refactorization
    * Added enzyme tests

--- src/components/SessionInput/SessionInput.test.tsx
+++ src/components/SessionInput/SessionInput.test.tsx
@@ -1,3 +1,4 @@
+import { shallow } from "enzyme";
 import * as React from "react";
 import * as renderer from "react-test-renderer";
 
@@ -12,5 +13,29 @@ describe("SessionInput", () => {
       .toJSON();
 
     expect(component).toMatchSnapshot();
+  });
+
+  it("should tell to press ENTER", () => {
+    const wrapper = shallow(<SessionInput handleSubmit={mockHandleSubmit} />);
+    wrapper.find("#session-code-input").simulate("change", {
+      target: { name: "session-code-input", value: "1234" }
+    });
+
+    expect(
+      wrapper.find("#session-code-input").prop("helperText")
+    ).toStrictEqual("and press ENTER.");
+  });
+
+  it("should tell to wait to fill code", () => {
+    const wrapper = shallow(
+      <SessionInput length={6} handleSubmit={mockHandleSubmit} />
+    );
+    wrapper.find("#session-code-input").simulate("change", {
+      target: { name: "session-code-input", value: "1234" }
+    });
+
+    expect(
+      wrapper.find("#session-code-input").prop("helperText")
+    ).toStrictEqual("Enter 6 characters code.");
   });
 });
