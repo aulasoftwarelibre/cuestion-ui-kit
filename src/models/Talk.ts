8d9c9c278f0a85fdeb73bed94c4cde79b7d5e0fc
Sergio Gómez <sergio@uco.es>
2019-07-04 00:24:42 +0200

    Added TalkItem component

---
+++ src/models/Talk.ts
@@ -0,0 +1,11 @@
+export interface Talk {
+    avatar?: string;
+    description: string;
+    id: string;
+    room: string;
+    title: string;
+    startsAt: Date;
+    endsAt: Date;
+    speaker: string;
+    topics: string[];
+}
