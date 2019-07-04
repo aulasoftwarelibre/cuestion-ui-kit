72ab0e95edb399ba4908160c29d0d967df8acbbd
Sergio Gómez <sergio@uco.es>
2019-07-04 20:29:06 +0200

    Updated jest config

--- src/internal/jest-setup.ts
+++ src/internal/jest-setup.ts
@@ -1,14 +1,6 @@
 import "jest-styled-components";
-import { JSDOM } from "jsdom";
-import "jsdom-global/register";
 
 import * as Enzyme from "enzyme";
-import * as Adapter from "enzyme-adapter-react-16";
+import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new Adapter() });
-
-const global: any = {};
-
-const doc = new JSDOM("<!doctype html><html><body></body></html>");
-global.document = doc;
-global.window = doc.defaultView;
