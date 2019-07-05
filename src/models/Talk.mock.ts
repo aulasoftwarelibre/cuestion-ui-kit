675ccf7f400ca6b272679dad92540c6bdfeedc41
Sergio Gómez <sergio@uco.es>
2019-07-05 19:12:26 +0200

    Added faker

--- src/models/Talk.mock.ts
+++ src/models/Talk.mock.ts
@@ -1,15 +1,47 @@
+import faker from "faker";
+
 import Talk from "./Talk";
 
-export const talk: Talk = {
-  avatar: "https://i.pravatar.cc/150?img=22",
-  description:
-    // tslint:disable-next-line: max-line-length
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan quis neque at dignissim. Donec auctor bibendum quam sed elementum. Sed gravida, arcu porta sollicitudin porttitor, nisl ligula placerat massa, eu fermentum magna sapien eu sem. Nulla facilisi. Aliquam erat volutpat",
-  endsAt: new Date("2019/09/13 10:20:00 GMT"),
-  id: "1",
-  room: "Room 1",
-  speaker: "John Doe",
-  startsAt: new Date("2019/09/13 09:30:00 GMT"),
-  title: "My awesome talk",
-  topics: ["Mobile", "Web"]
-};
+function createTalk(
+  id: string,
+  title: string,
+  room: string,
+  startsAt: Date,
+  topics: string[]
+): Talk {
+  faker.seed(Number(id));
+
+  const endsAt = new Date(startsAt.getTime());
+  endsAt.setMinutes(startsAt.getMinutes() + 50);
+
+  return {
+    avatar: faker.image.avatar(),
+    description: faker.lorem.paragraph(),
+    endsAt,
+    id,
+    room,
+    speaker: faker.name.findName(),
+    startsAt,
+    title,
+    topics
+  };
+}
+
+export const talk: Talk = createTalk(
+  "1",
+  "My awesome talk",
+  "Room 1",
+  new Date("2019/09/13 09:30:00 GMT"),
+  ["Mobile", "Web"]
+);
+
+export const talks: Talk[] = [
+  talk,
+  createTalk(
+    "2",
+    "My amazing talk",
+    "Room 2",
+    new Date("2019/09/13 09:30:00 GMT"),
+    ["Frontend", "Agile"]
+  )
+];
