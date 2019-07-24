a4fb3d204bf6d21723dfe89cb60065edb1eef27b
adrianlopezgue <adrian.lopez.gue@hotmail.com>
2019-07-24 14:38:28 +0200

    QuestionItem created

---
+++ src/components/QuestionItem/QuestionItem.stories.tsx
@@ -0,0 +1,13 @@
+import { action } from "@storybook/addon-actions";
+import {object, withKnobs} from "@storybook/addon-knobs";
+import { storiesOf } from "@storybook/react";
+import * as React from "react";
+
+import { question } from "../../models/Question.mock";
+import { wInfo } from "../../utils/wInfo";
+import QuestionItem from "./QuestionItem";
+
+const stories = storiesOf("Components/QuestionItem", module);
+stories.addDecorator(withKnobs);
+
+stories.add("default", wInfo()(() => <QuestionItem onClickHandler={action("question")} question={object("Question", question)} />));
---
+++ src/components/QuestionItem/QuestionItem.tsx
@@ -0,0 +1,87 @@
+import { Avatar, Card, CardActions, CardContent, CardHeader, IconButton, Typography } from "@material-ui/core";
+import { red } from "@material-ui/core/colors";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+
+import * as React from "react";
+// import { FormattedMessage, InjectedIntl, injectIntl } from "react-intl";
+import { Question } from "../../models/Question";
+
+// import messages from "../../languages/messages";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    card: {
+      width: 100
+    },
+    media: {
+      height: 0,
+      paddingTop: "56.25%" // 16:9
+    },
+    expand: {
+      transform: "rotate(0deg)",
+      marginLeft: "auto",
+      transition: theme.transitions.create("transform", {
+        duration: theme.transitions.duration.shortest
+      })
+    },
+    expandOpen: {
+      transform: "rotate(180deg)"
+    },
+    avatar: {
+      backgroundColor: red[500]
+    }
+  })
+);
+
+interface Props {
+    onClickHandler: any;
+  // intl: InjectedIntl;
+  question: Question;
+}
+
+const _QuestionItem: React.FunctionComponent<Props> = ({ onClickHandler, question }) => {
+  const [pressed, setPressed] = React.useState<boolean>(false);
+
+  function handleLikeButton() {
+    onClickHandler(question.id);
+    setPressed(!pressed);
+    question.isVoted = !pressed;
+    if (question.isVoted) {
+      question.votes = question.votes - 1;
+    } else {
+      question.votes = question.votes + 1;
+    }
+  }
+
+  const classes = useStyles();
+
+  return (
+    <Card>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="User avatar" className={classes.avatar}>
+            X
+          </Avatar>
+        }
+        title={question.username}
+        subheader={question.createdAt.toUTCString()}
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {question.question}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="Like button" onClick={() => handleLikeButton()}>
+          <FavoriteIcon />
+        </IconButton>
+        {question.votes}
+      </CardActions>
+    </Card>
+  );
+};
+
+// export const QuestionItem = injectIntl(_QuestionItem);
+
+export default _QuestionItem;
