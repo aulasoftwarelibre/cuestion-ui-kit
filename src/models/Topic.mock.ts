38ee450258ddd442c8701f0401e0588956fdba67
josefrnandezz <i62fealj@uco.es>
2019-07-25 14:34:11 +0200

    Updated interface Topic

---
+++ src/models/Topic.mock.ts
@@ -0,0 +1,13 @@
+import Topic from "./Topic";
+
+function createTopic(label: string): Topic {
+  return {
+    label,
+  };
+}
+
+export const topics: Topic[] = [
+  createTopic("Agile"),
+  createTopic("Backend"),
+  createTopic("Frontend"),
+];
