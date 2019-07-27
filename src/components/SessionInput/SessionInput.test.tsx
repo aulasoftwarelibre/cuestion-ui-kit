9974de4717cd4949e04cec656e8c4e610ffa77f2
Sergio Gómez <sergio@uco.es>
2019-07-27 20:28:49 +0200

    Added test for SessionInput mock handler (#15)
    * Read code directly from state
    * Tested session input handler result

--- src/components/SessionInput/SessionInput.test.tsx
+++ src/components/SessionInput/SessionInput.test.tsx
@@ -16,12 +16,11 @@ describe("SessionInput", () => {
 
   it("should tell to press ENTER", () => {
     const wrapper = mountWithIntl(<SessionInput handleOnSubmit={mockOnHandleSubmit} length={4} />);
-    wrapper
-      .find(TextField)
-      .find("input")
-      .simulate("change", {
-        target: { name: "session-code-input", value: "1234" }
-      });
+    const input = wrapper.find(TextField).find("input");
+
+    input.simulate("change", {
+      target: { name: "session-code-input", value: "1234" }
+    });
 
     expect(wrapper.find(TextField).prop("helperText")).toStrictEqual("and press ENTER.");
   });
@@ -36,5 +35,19 @@ describe("SessionInput", () => {
       });
 
     expect(wrapper.find(TextField).prop("helperText")).toStrictEqual("Enter 6 characters code.");
+  });
+
+  it("should return session CODE", () => {
+    const wrapper = mountWithIntl(<SessionInput handleOnSubmit={mockOnHandleSubmit} length={4} />);
+    const input = wrapper.find(TextField).find("input");
+    const form = wrapper.find("form");
+
+    input.simulate("change", {
+      target: { name: "session-code-input", value: "1234" }
+    });
+    form.simulate("submit");
+
+    expect(mockOnHandleSubmit).toHaveBeenCalledTimes(1);
+    expect(mockOnHandleSubmit).toHaveBeenCalledWith("1234");
   });
 });
