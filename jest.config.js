1aa001815c6d86088d3665ba18685ed1b3fb4979
Sergio Gómez <sergio@uco.es>
2019-07-28 22:45:10 +0200

    Moved tests to root

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  setupFilesAfterEnv: ["./internal/jest-setup.ts"],
+  globals: { "ts-jest": { diagnostics: { ignoreCodes: [151001] } } },
+  globalSetup: "./internal/global-setup.js",
+  roots: ["<rootDir>packages"],
+  testMatch: ["**/__tests__/*.+(ts|tsx|js)", "**/*.test.+(ts|tsx|js)"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest"
+  }
+};
--- packages/ui/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  rootDir: "src",
-  testEnvironment: "node",
-  setupFilesAfterEnv: ["./internal/jest-setup.ts"],
-  globals: { "ts-jest": { diagnostics: { ignoreCodes: [151001] } } },
-  globalSetup: "./internal/global-setup.js"
-};
-
