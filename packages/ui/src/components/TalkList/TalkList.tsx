d9506dfe3e78093e2606ed5adc8e366bde482856
josefrnandezz <i62fealj@uco.es>
2019-07-30 20:27:52 +0200

    Fixed CSS

--- packages/ui/src/components/TalkList/TalkList.tsx
+++ packages/ui/src/components/TalkList/TalkList.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 import { Grid, Typography } from "@material-ui/core";
+import { createStyles, Theme, makeStyles } from "@material-ui/core/styles";
 import Talk from "../../models/Talk";
 import TalkItem from "../TalkItem/TalkItem";
 
@@ -11,14 +12,30 @@ export interface Props {
   title: string;
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+      overflow: 'hidden',
+      padding: theme.spacing(0, 1)
+    },
+    grid: {
+      padding: theme.spacing(2),
+    },
+  })
+);
+
 export const TalkList: React.FunctionComponent<Props> = ({ handleOnClick, filter, talks, title }) => {
+  const classes = useStyles();
+
   const list = talks.map(talk => (
-    <Grid item xs={6} md={4} key={talk.id}>
+    <Grid item xs={12} justify="flex-start" md={4} key={talk.id} className={classes.grid}>
       <TalkItem handleOnClick={handleOnClick} talk={talk} raised={filter.some(r => talk.topics.includes(r))} />
     </Grid>
   ));
+
   return (
-    <div>
+    <div className={classes.root}>
       <Grid container spacing={2}>
         <Grid item key="title" xs={12}>
           <Typography component="h2" variant="h6" color="primary">
