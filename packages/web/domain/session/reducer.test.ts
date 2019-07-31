c32041ac5c80823b3f8684b659948ff9ca1c023a
Sergio Gómez <sergio@uco.es>
2019-07-31 22:28:36 +0200

    CS

--- packages/web/domain/session/reducer.test.ts
+++ packages/web/domain/session/reducer.test.ts
@@ -4,7 +4,6 @@ import { reducer } from "./reducer";
 import * as types from "./types";
 
 describe("session reducer", () => {
-
   it("should return the initial state", () => {
     // tslint:disable-next-line:no-object-literal-type-assertion
     const action = {} as types.SessionActionTypes;
@@ -13,16 +12,14 @@ describe("session reducer", () => {
   });
 
   it("should handle OPEN_SESSION_SUCCESS", () => {
-    const session : Session = {
-      id: '1',
-      code: '1234',
-      title: 'My session'
+    const session: Session = {
+      id: "1",
+      code: "1234",
+      title: "My session"
     };
 
-    expect(
-      reducer(undefined, openSessionSuccess(session))
-    ).toEqual({
+    expect(reducer(undefined, openSessionSuccess(session))).toEqual({
       session
-    })
-  })
+    });
+  });
 });
