7149724bfe9d9bb76a77609f9314bcdadb77cf46
josefrnandezz <i62fealj@uco.es>
2019-08-01 01:39:49 +0200

    Added tests to QuestionList

---
+++ packages/ui/src/components/QuestionList/QuestionList.test.tsx
@@ -0,0 +1,25 @@
+import IconButton from "@material-ui/core/IconButton";
+
+import * as React from "react";
+
+import { questions } from "../../models/Question.mock";
+import { createComponentWithIntl, mountWithIntl } from "../../utils/createComponentWithIntl";
+import QuestionList from "./QuestionList";
+
+describe("QuestionList", () => {
+  const mockOnHandleClick = jest.fn();
+
+  it("should render correctly", () => {
+    const component = createComponentWithIntl(<QuestionList handleOnClick={mockOnHandleClick} questions={questions} title={"Question List"} />).toJSON();
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it("should return message from voted question", () => {
+    const wrapper = mountWithIntl(<QuestionList handleOnClick={mockOnHandleClick} questions={questions} title={"Question List"} />);
+
+    wrapper.find(IconButton).first().simulate("click");
+
+    expect(mockOnHandleClick).toBeCalledWith(questions[0].id, !questions[0].isVoted);
+  });
+});
