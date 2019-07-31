c685adebd17e25a80894870e147973915866d484
josefrnandezz <i62fealj@uco.es>
2019-08-01 01:40:41 +0200

    Added tests to TalkList

---
+++ packages/ui/src/components/TalkList/TalkList.test.tsx
@@ -0,0 +1,30 @@
+import * as React from "react";
+
+import { talks } from "../../models/Talk.mock";
+import { createComponentWithIntl, mountWithIntl } from "../../utils/createComponentWithIntl";
+import TalkList from "./TalkList";
+import CardActionArea from "@material-ui/core/CardActionArea";
+
+describe("TalkList", () => {
+  const mockOnHandleClick = jest.fn();
+
+  it("should render correctly without filter", () => {
+    const component = createComponentWithIntl(<TalkList handleOnClick={mockOnHandleClick} filter={[]} talks={talks} title={"Talk List"} />).toJSON();
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it("should render correctly with filter", () => {
+    const component = createComponentWithIntl(<TalkList handleOnClick={mockOnHandleClick} filter={["Agile"]} talks={talks} title={"Talk List"} />).toJSON();
+
+    expect(component).toMatchSnapshot();
+  });
+
+  it("should return message from talk selected", () => {
+    const wrapper = mountWithIntl(<TalkList handleOnClick={mockOnHandleClick} filter={[]} talks={talks} title={"Talk List"} />);
+
+    wrapper.find(CardActionArea).first().simulate("click");
+
+    expect(mockOnHandleClick).toBeCalledWith(talks[0].id);
+  });
+});
