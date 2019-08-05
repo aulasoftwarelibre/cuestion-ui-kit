1c1bf8c0cd41ad3dcdcded431a1d3a92276153b9
Sergio Gómez <sergio@uco.es>
2019-08-05 22:45:17 +0200

    [web] Removed styled components

--- packages/web/pages/sessions/[code].tsx
+++ packages/web/pages/sessions/[code].tsx
@@ -1,25 +1,105 @@
-import axios from "axios";
+import { TalkList } from "@cuestion/ui";
+import Talk from "@cuestion/ui/dist/models/Talk";
+import {
+  AppBar,
+  Container,
+  Grid,
+  IconButton,
+  Paper,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import MenuIcon from "@material-ui/icons/Menu";
+import fetch from "isomorphic-unfetch";
 import { NextPage } from "next";
+import { Session } from "../../src/session/types";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: "flex",
+    },
+    appBarSpacer: theme.mixins.toolbar,
+    content: {
+      flexGrow: 1,
+      height: "100vh",
+      overflow: "auto",
+    },
+    container: {
+      paddingTop: theme.spacing(4),
+      paddingBottom: theme.spacing(4),
+    },
+    paper: {
+      padding: theme.spacing(2),
+      display: "flex",
+      overflow: "auto",
+      flexDirection: "column",
+    },
+    fixedHeight: {
+      height: 240,
+    },
+  }),
+);
 
 interface Props {
   code?: string | string[];
+  session: Session;
+  talks: Talk[];
 }
 
-const SessionPage: NextPage<Props> = ({ code }) => (
-  <main>Your code: {code}</main>
-);
+const SessionPage: NextPage<Props> = ({ code, session, talks }) => {
+  const classes = useStyles({});
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="absolute">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="open drawer">
+            <MenuIcon />
+          </IconButton>
+          <Typography component="h1" variant="h6" color="inherit" noWrap>
+            {session.title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main className={classes.content}>
+        <div className={classes.appBarSpacer} />
+        <Container maxWidth="lg" className={classes.container}>
+          <Grid container spacing={3}>
+            <TalkList
+              filter={[]}
+              talks={talks}
+              title={session.title}
+              handleOnClick={() => true}
+            />
+          </Grid>
+        </Container>
+      </main>
+    </div>
+  );
+};
 
 SessionPage.getInitialProps = async ({ query }) => {
   const code =
     typeof query.code === "string" ? query.code : query.code.join("");
 
-  const sessionContent = await axios.get(
+  const sessionRequest = await fetch(
     `${process.env.NEXT_PUBLIC_API_HOST}/sessions/${encodeURIComponent(
       code,
     )}/code`,
   );
+  const session = await sessionRequest.json();
 
-  return { code };
+  const talksRequest = await fetch(
+    `${process.env.NEXT_PUBLIC_API_HOST}/sessions/${encodeURIComponent(
+      code,
+    )}/talks`,
+  );
+
+  const talks = await talksRequest.json();
+
+  return { code, session, talks };
 };
 
 export default SessionPage;
