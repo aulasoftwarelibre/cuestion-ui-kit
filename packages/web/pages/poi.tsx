2b73928f30eda4b7b3e76cf219196e9aa4ecadbe
Sergio Gómez <sergio@uco.es>
2019-09-12 16:50:51 +0200

    Added agenda pages

---
+++ packages/web/pages/poi.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next";
+import * as React from "react";
+
+import BottomMenu from "../components/BottomMenu";
+
+const PoiPage: NextPage = () => {
+  return (
+    <>
+      Hola
+      <BottomMenu value={"poi"} />
+    </>
+  );
+};
+
+export default PoiPage;
