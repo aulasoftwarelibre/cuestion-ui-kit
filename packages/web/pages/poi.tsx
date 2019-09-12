6e116556a12203ddad7e8ab15538b8ac14cae8ce
josefrnandezz <i62fealj@uco.es>
2019-09-12 21:12:32 +0200

    CS

--- packages/web/pages/poi.tsx
+++ packages/web/pages/poi.tsx
@@ -1,11 +1,27 @@
+import {
+  AppBar,
+  Avatar,
+  Button,
+  Chip,
+  Grid,
+  IconButton,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemSecondaryAction,
+  ListItemText,
+  ListSubheader,
+  Paper,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import { NextPage } from "next";
 import * as React from "react";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import { AppBar, Avatar, Paper, Toolbar, Typography, Grid, List, ListSubheader, ListItem, ListItemText, ListItemIcon, ListItemSecondaryAction, Button, Chip, IconButton } from "@material-ui/core";
 
-import BottomMenu from "../components/BottomMenu";
 import { Session } from "@cuestion/ui";
 import { LocationOn } from "@material-ui/icons";
+import BottomMenu from "../components/BottomMenu";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -23,45 +39,47 @@ const useStyles = makeStyles((theme: Theme) =>
       padding: theme.spacing(5, 2),
     },
     list: {
-      width: '100%',
+      width: "100%",
       maxWidth: 360,
       backgroundColor: theme.palette.background.paper,
-      paddingTop: "80px"
+      paddingTop: "80px",
     },
     container: {
       display: "flex",
       alignItems: "flex-start",
       justifyContent: "center",
       flexWrap: "wrap",
-      marginBottom: "15%"
-    }, 
+      marginBottom: "15%",
+    },
     grid: {
       padding: theme.spacing(0),
-    }
+    },
   }),
 );
 
 interface Props {
-  session: Session,
-  poi
+  session: Session;
+  poi;
 }
 
 const PoiPage: NextPage<Props> = ({ session, poi }) => {
-  const classes = useStyles({}); 
+  const classes = useStyles({});
 
-  const mapsSelector = (location) => {
-    if ((navigator.platform.indexOf("iPhone") != -1) || 
-     (navigator.platform.indexOf("iPad") != -1) || 
-     (navigator.platform.indexOf("iPod") != -1)) {
+  const mapsSelector = location => {
+    if (
+      navigator.platform.indexOf("iPhone") != -1 ||
+      navigator.platform.indexOf("iPad") != -1 ||
+      navigator.platform.indexOf("iPod") != -1
+    ) {
       window.open(location);
-     } else { 
+    } else {
       window.open(location);
-     }
-  }
-  
+    }
+  };
+
   return (
     <>
-    <AppBar color="primary" position="absolute">
+      <AppBar color="primary" position="absolute">
         <Toolbar>
           <Avatar src={session.logo} style={{ marginRight: "5px" }} />
           <Typography component="h1" variant="h6" color="inherit" noWrap>
@@ -70,42 +88,60 @@ const PoiPage: NextPage<Props> = ({ session, poi }) => {
         </Toolbar>
       </AppBar>
       <div className={classes.container}>
-      <List subheader={<ListSubheader>Congreso</ListSubheader>} className={classes.list}>
-        {poi.conference.map(point =>(
+        <List
+          subheader={<ListSubheader>Congreso</ListSubheader>}
+          className={classes.list}>
+          {poi.conference.map(point => (
             <ListItem>
-              <ListItemText id={point.id} primary={point.title} secondary={point.description}/>
+              <ListItemText
+                id={point.id}
+                primary={point.title}
+                secondary={point.description}
+              />
               <ListItemSecondaryAction>
                 <IconButton onClick={() => mapsSelector(point.url)}>
-                  <LocationOn/>
+                  <LocationOn />
                 </IconButton>
               </ListItemSecondaryAction>
             </ListItem>
-        ))}
-      </List>
-      <List subheader={<ListSubheader>Desayuno</ListSubheader>} className={classes.list}>
-        {poi.breakfast.map(point =>(
-              <ListItem>
-                <ListItemText id={point.id} primary={point.title} secondary={point.description}/>
-                <ListItemSecondaryAction>
-                  <IconButton onClick={() => mapsSelector(point.url)}>
-                    <LocationOn/>
-                  </IconButton>
-                </ListItemSecondaryAction>
-              </ListItem>
-        ))}
-      </List>
-      <List subheader={<ListSubheader>Almuerzo</ListSubheader>} className={classes.list}>
-        {poi.lunch.map(point =>(
+          ))}
+        </List>
+        <List
+          subheader={<ListSubheader>Desayuno</ListSubheader>}
+          className={classes.list}>
+          {poi.breakfast.map(point => (
             <ListItem>
-              <ListItemText id={point.id} primary={point.title} secondary={point.description}/>
+              <ListItemText
+                id={point.id}
+                primary={point.title}
+                secondary={point.description}
+              />
               <ListItemSecondaryAction>
                 <IconButton onClick={() => mapsSelector(point.url)}>
-                  <LocationOn/>
+                  <LocationOn />
                 </IconButton>
               </ListItemSecondaryAction>
             </ListItem>
-        ))}
-      </List>
+          ))}
+        </List>
+        <List
+          subheader={<ListSubheader>Almuerzo</ListSubheader>}
+          className={classes.list}>
+          {poi.lunch.map(point => (
+            <ListItem>
+              <ListItemText
+                id={point.id}
+                primary={point.title}
+                secondary={point.description}
+              />
+              <ListItemSecondaryAction>
+                <IconButton onClick={() => mapsSelector(point.url)}>
+                  <LocationOn />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          ))}
+        </List>
       </div>
       <BottomMenu value={"poi"} />
     </>
